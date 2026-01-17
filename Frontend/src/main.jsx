483c4b4c00ce8e836d9052392152774aa16dc3eb
Muraydevs1 <139182142+Muraydevs1@users.noreply.github.com>
2026-01-17 08:37:15 +0000

    building 1st Eccom-website

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react'
+import ShopContextProvider from './context/ShopContext.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+     <ShopContextProvider>
+       <App />
+     </ShopContextProvider>
+    </BrowserRouter>
+  </StrictMode>
+)
+ 
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import LatestCollection from '../components/latestCollection' 
+
+function Home() {
+  return (
+    <div>
+      <Hero/>
+      <LatestCollection/>
+    </div>
+  )
+}
+
+export default Home
+
