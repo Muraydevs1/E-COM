539f220d749657c5ace5a60a3df094dd435fa764
Muraydevs1 <139182142+Muraydevs1@users.noreply.github.com>
2026-01-24 23:23:35 +0000

    new additions

--- Frontend/src/pages/Collection.jsx
+++ Frontend/src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from '../components/Title';
 import ProductItem from '../components/ProductItem';
 
 function Collection() {
-  const {products} = useContext(ShopContext);
+  const {products, search, showSearch} = useContext(ShopContext);
   const [showFilter, setshowFilter] = useState(false);
   const [filterProducts, setfilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -30,6 +30,11 @@ function Collection() {
 
     const applyFilter = () => {
       let productsCopy = products.slice();
+
+      if(search && showSearch){
+        productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+
+      }
       if (category.length > 0){
         productsCopy = productsCopy.filter(item=>category.includes(item.category))
       }
@@ -68,7 +73,7 @@ function Collection() {
 
     useEffect(()=>{
       applyFilter();
-    },[category, subCategory])
+    },[category, subCategory, search, showSearch])
 
     useEffect(()=>{
       sortProduct();
